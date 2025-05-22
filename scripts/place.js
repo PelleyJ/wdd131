3c766daf9f14121dd064661f8f47c4e3ce804157
PelleyJ <pelleysam@gmail.com>
2025-05-23 02:13:33 +0900

    Modified to "Japan"

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,5 @@
-const temp = 10;
-const wind = 5;
+const temp = 17; // °C
+const wind = 9;  // km/h
 
 function calculateWindChill(t, s) {
   return (13.12 + 0.6215 * t - 11.37 * Math.pow(s, 0.16) + 0.3965 * t * Math.pow(s, 0.16)).toFixed(1);
@@ -12,6 +12,6 @@ if (temp <= 10 && wind > 4.8) {
   windChillOutput.textContent = 'N/A';
 }
 
-// Footer year and last modified
+// Footer dynamic year + last modified
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
